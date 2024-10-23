4d35dba09b158e4be6f3acd1abf9c25a552f062d
brianaqp <brianaqp@MB-B.local>
2024-10-23 14:22:17 +0900

    feat: seeds implemented

--- db/seeds/0_clear.js
+++ db/seeds/0_clear.js
@@ -1,13 +1,20 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+exports.seed = async function (knex) {
+  const autoIncrementFields = [
+    "modules_id_seq",
+    "categories_id_seq",
+    "users_id_seq",
+  ];
+
+  // Reset to 1
+  for (const value of autoIncrementFields) {
+    await knex.raw("ALTER SEQUENCE public." + value + " RESTART WITH 1");
+  }
+
+  await knex("modules").del();
+  await knex("categories").del();
+  await knex("users").del();
 };
