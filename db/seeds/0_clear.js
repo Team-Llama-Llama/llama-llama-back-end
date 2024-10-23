2c123ab72e5838e092ce4b443d66ee47a6e03cdc
brianaqp <brianaqp@MB-B.local>
2024-10-23 16:12:18 +0900

    feat: db on delete cascade

--- db/seeds/0_clear.js
+++ db/seeds/0_clear.js
@@ -3,17 +3,19 @@
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
+  // Autoincrementers for IDS en every table
   const autoIncrementFields = [
     "modules_id_seq",
     "categories_id_seq",
     "users_id_seq",
   ];
 
-  // Reset to 1
+  // Reset to every table id to 1
   for (const value of autoIncrementFields) {
     await knex.raw("ALTER SEQUENCE public." + value + " RESTART WITH 1");
   }
 
+  // Delete data
   await knex("modules").del();
   await knex("categories").del();
   await knex("users").del();
